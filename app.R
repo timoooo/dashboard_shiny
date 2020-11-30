d77cdb4571d07a51325d1c3ba4931140475fb260
Timo <timo.hasenbichler@protonmail.com>
2020-11-30 21:57:10 +0100

    Adds overall plot

--- app.R
+++ app.R
@@ -3,82 +3,95 @@ library(shinydashboard)
 library(tidyverse)
 library(ggthemes)
 
-get_countries<-function(df){
-  df<-df%>%select(country)%>% unique() %>% arrange(country)
+get_countries <- function(df) {
+  df <- df %>% select(country) %>% unique() %>% arrange(country)
   return (df)
 }
 
-get_food_cat<-function(df){
-  df<-df%>%select(food_category)%>% unique() %>% arrange(country)
+get_food_cat <- function(df) {
+  df <- df %>% select(food_category) %>% unique() %>% arrange(country)
   return (df)
 }
 
-df <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-02-18/food_consumption.csv')
-
+df <-
+  readr::read_csv(
+    'https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-02-18/food_consumption.csv'
+  )
+df <-
+  df %>% mutate(cons_to_emiss_ratio = consumption / co2_emmission)
 
 ui <- dashboardPage(
   dashboardHeader(title = "Food consumption dashboard"),
-  dashboardSidebar(    sidebarMenu(
-    menuItem("Plot by nation", tabName = "plotbynation", icon = icon("dashboard")),
-    menuItem("Plot by food category", tabName = "plotbyfoodcat", icon = icon("th"))
+  dashboardSidebar(sidebarMenu(
+    menuItem(
+      "Plot consumption",
+      tabName = "plothome",
+      icon = icon("dashboard")
+    ),
+    menuItem("Plot by nation",
+             tabName = "plotbynation",
+             icon = icon("th"))
   )),
-  dashboardBody(
-    tabItems(
-      # First tab content
-      tabItem(tabName = "plotbynation",
-              h2("Consumption of food by Country"),
-              
-              fluidRow(
-                box(plotOutput("plot1", height = 500)),
-                
-                box(
-                  title = "Select a country",
-                  selectInput("country", "Country:",
-                              choices = get_countries(df),selected = "Austria")
-                )
-              )
-      )
+  dashboardBody(tabItems(
+    # First tab content
+    tabItem(
+      tabName = "plotbynation",
+      h2("Consumption of food by Country"),
       
-      # Second tab content
-      # tabItem(tabName = "plotbyfoodcat",
-      #         h2("Consumption of food by food category"),
-      #         fluidRow(
-      #           box(plotOutput("plot2", height = 500)),
-      #           box(title = "Select a category",
-      #               selectInput("foodcat","Food category: ",
-      #                           choices = get_food_cat(df),
-      #                           selected = "Beef"
-      #                           )))
-     # )
+      fluidRow(
+        box(plotOutput("plot1", height = 500)),
+        
+        box(
+          title = "Select a country",
+          selectInput(
+            "country",
+            "Country:",
+            choices = get_countries(df),
+            selected = "Austria"
+          )
+        ),
+        box(
+          title = "Select consumption or co2 emissions",
+          selectInput(
+            "type",
+            "Consumptions or Emissions",
+            choices = c("co2 Emmission" = "co2_emmission",
+                        "consumption" = "consumption"),
+            selected = "consumption"
+          )
+        )
+      )
+    ),
+    tabItem(
+      tabName = "plothome",
+      h2("Consumption of food all countries"),
+      fluidRow(box(plotOutput("homeplot")))
     )
-  )
+  ))
 )
 server <- function(input, output) {
-  set.seed(122)
-  
-  
-    output$plot1<-renderPlot({
-    df<-food_consumption%>%filter(country==input$country) 
-    ggplot(df,aes(food_category,consumption,fill=food_category))+
-      geom_bar(stat="identity")+
-      theme(axis.title.x=element_blank(),
-            axis.text.x=element_blank(),
-            axis.ticks.x=element_blank(),
-            legend.title=element_blank())
+  output$plot1 <- renderPlot({
+    df <-
+      food_consumption %>% filter(country == input$country) %>% arrange(!!as.name(input$type))
+    ggplot(df, aes(food_category,!!as.name(input$type), fill = food_category)) +
+      geom_bar(stat = "identity") +
+      theme(
+        axis.title.x = element_blank(),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        legend.title = element_blank()
+      )
   })
   
-    output$plot2<-renderPlot({
-      df<-food_consumption%>%filter(country==input$country) 
-      ggplot(df,aes(food_category,consumption,fill=food_category))+
-        geom_bar(stat="identity")+
-        theme(axis.title.x=element_blank(),
-              axis.text.x=element_blank(),
-              axis.ticks.x=element_blank(),
-              legend.title=element_blank())
-    })
+  output$homeplot <- renderPlot({
+    df %>%   ggplot(aes(x = fct_reorder(food_category, consumption), y = consumption, color = country)) +
+      geom_jitter() +
+      theme(legend.position = "none") +
+      coord_flip()
     
+  })
+  
   
 }
 
 shinyApp(ui, server)
-
--- dashboard_tools.R
+++ dashboard_tools.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 library(ggplot2)
-
+#https://github.com/rfordatascience/tidytuesday/blob/master/data/2020/2020-02-18/readme.md
 get_countries<-function(df){
   df<-df%>%select(country)%>% unique()
   return (df)
@@ -9,22 +9,35 @@ get_countries<-function(df){
 
 
 food_consumption %>%group_by(food_category)
+df <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-02-18/food_consumption.csv')
 
 
 df<-food_consumption%>%filter(country=="Argentina") 
-
-ggplot(df,aes(food_category,consumption,fill=food_category)) +
+df
+ggplot(df,aes(food_category,co2_emmission,fill=food_category)) +
   geom_bar(stat="identity") +
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank())
+
+df <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-02-18/food_consumption.csv')
+
+df<-food_consumption%>%filter(food_category=="Beef") %>% arrange(desc(consumption))
+df
+ggplot(df,aes(country,consumption)) +
+  geom_point() +
   theme(axis.title.y=element_blank(),
         axis.text.y=element_blank(),
         axis.ticks.y=element_blank())
 
 df <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-02-18/food_consumption.csv')
+df <- df %>% mutate(cons_to_emiss_ratio=co2_emmission/consumption)
+df
 
-df<-food_consumption%>%filter(food_category=="Beef") %>% arrange(desc(consumption))
+df%>%filter(country=="Austria")
 
-ggplot(df,aes(country,consumption,fill=country)) +
-  geom_point() +
-  theme(axis.title.y=element_blank(),
-        axis.text.y=element_blank(),
-        axis.ticks.y=element_blank())
+
+df %>%   ggplot(aes(x = fct_reorder(food_category, consumption), y = consumption, color = country)) +
+  geom_jitter() +
+  theme(legend.position = "none") +
+  coord_flip()
